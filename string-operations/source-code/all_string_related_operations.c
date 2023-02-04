a706598073e94229e3c1178b449c05a6ea9e2a39
Ritesh Navadiya <riteshnavadiya111@gmail.com>
2023-02-04 13:05:40 +0530

    complete assessment

--- string-operations/source-code/all_string_related_operations.c
+++ string-operations/source-code/all_string_related_operations.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-// 1)--------------------------------------
+// 1)----------------------------------------------------------------------------------------------
 int epuality()
 {
     printf("------------------------------------------------------------------------\n");
     printf("\n1) Epuality Funciton :-\n");
     char arr1[100], arr2[100];
     printf("Enter 1st string : ");
-    gets(arr1);
-    printf("\nEnter 2nd string : ");
-    gets(arr2);
+    scanf(" %s", arr1);
+    printf("Enter 2nd string : ");
+    scanf(" %s", arr2);
     if (strcmp(arr1, arr2) == 0)
-        printf("\t- Strings are equal");
+        printf("\n\t- Strings are equal");
     else
-        printf("\t- Strings are not equal");
+        printf("\n\t- Strings are not equal");
+    printf("\n------------------------------------------------------------------------\n");
     return 0;
 }
-// 2) -------------------------------------
+// 2) ------------------------------------------------------------------------------------------------
 int stringCopy()
 {
     printf("\n------------------------------------------------------------------------\n");
@@ -25,30 +26,33 @@ int stringCopy()
     printf("\n2) StringCopy Funciton :-\n");
     char sent[100], copy_sent[100];
     printf("Enter Senetence : ");
-    gets(sent);
+    scanf(" %s", sent);
     printf("Your String : %s", sent);
     strcpy(copy_sent, sent);
-    printf("\nString Copy : %s", copy_sent);
-    return 0;
+    printf("\t\n\tString Copy : %s", copy_sent);
+    printf("\n------------------------------------------------------------------------\n");
 
     return 0;
 }
-// 3) -----------------------------------
+// 3) ------------------------------------------------------------------------------------------------
 int concat()
 {
     printf("\n------------------------------------------------------------------------\n");
 
     printf("\n3) Concat Funciton :-\n");
-    char name[20], surname[20];
+    char name[20];
+    char surname[20];
     printf("Enter FastName : ");
-    gets(name);
+    scanf(" %s", name);
     printf("Enter LastName: ");
-    gets(surname);
+    scanf(" %s", surname);
     strcat(name, surname);
-    printf("%s", name, surname);
+    printf("\t\n\tYour Concat String : %s", name, surname);
+    printf("\n------------------------------------------------------------------------\n");
+
     return 0;
 }
-// 4) ---------------------------------
+// 4) ---------------------------------------------------------------------------------------------------
 int length_string()
 {
     printf("\n------------------------------------------------------------------------\n");
@@ -57,13 +61,14 @@ int length_string()
     char strl[100];
     printf("enter your string : ");
     scanf("%s", strl);
-    printf("%s", strl);
+    printf(" Your String : %s", strl);
     int lan = strlen(strl);
-    printf("Your String Length : %d", lan);
+    printf("\t\n\tYour String Length : %d", lan);
+    printf("\n------------------------------------------------------------------------\n");
 
     return 0;
 }
-// 5) ---------------------------------
+// 5) -------------------------------------------------------------------------------------------------------
 int reverse()
 {
     printf("\n------------------------------------------------------------------------\n");
@@ -71,19 +76,21 @@ int reverse()
     printf("\n5) Reverse Funciton :-\n");
     char str[100];
     printf("Enter Your String : ");
-    gets(str);
-    printf("After the reverse of a string: %s ", strrev(str));
+    scanf(" %s", str);
+    strrev(str);
+    printf("\t\n\tAfter the reverse of a string: %s ", str);
+    printf("\n------------------------------------------------------------------------\n");
 
     return 0;
 }
-// 6) ----------------------------------
+// 6) --------------------------------------------------------------------------------------------------------
 int palindrone()
 {
     printf("\n------------------------------------------------------------------------\n");
 
     printf("\n6) Palindrone Funciton :-\n");
     int n, r, sum = 0, temp;
-    printf("enter the number=");
+    printf("enter the number = ");
     scanf("%d", &n);
     temp = n;
     while (n > 0)
@@ -93,92 +100,148 @@ int palindrone()
         n = n / 10;
     }
     if (temp == sum)
-        printf("palindrome number ");
+        printf("\n\tpalindrome number ");
     else
-        printf("not palindrome");
+        printf("\n\tnot palindrome");
+    printf("\n------------------------------------------------------------------------\n");
+
     return 0;
 }
-// 7) ----------------------------------
-// int()
-// {
-//     printf("\n------------------------------------------------------------------------\n");
+// 7) --------------------------------------------------------------------------------------------------------
+int srequency_character_s_string()
+{
+    char str[1000], ch;
+    int count = 0;
 
-//     printf("\n7)   Funciton :-\n");
+    printf("Enter a string: ");
+    scanf("%s", str, sizeof(str), stdin);
 
-//     return 0;
-// }
-// 8) ---------------------------------
-// int ()
-// {    printf("\n------------------------------------------------------------------------\n");
+    printf("Enter a character to find its frequency: ");
+    scanf(" %c", &ch);
 
-//     printf("\n8) Funciton :-\n");
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        if (ch == str[i])
+            ++count;
+    }
 
-//     return 0;
-// }
+    printf("\tFrequency of %c = %d", ch, count);
+    printf("\n------------------------------------------------------------------------\n");
 
-// 9) ---------------------------------
-// int ()
-// {    printf("\n------------------------------------------------------------------------\n");
+    return 0;
+}
+// 8) ---------------------------------------------------------------------------------------------------------
+int number_blank_spaces_vowels_consonants_digits()
+{
 
-//     printf("\n8)  Funciton :-\n");
+    char line[150];
+    int vowels, consonant, digit, space;
 
-//     return 0;
-// }
-// main ---------------------------------
+    // initialize all variables to 0
+    vowels = consonant = digit = space = 0;
+
+    // get full line of string input
+    printf("Enter a line of string: ");
+    scanf(" %s", line, sizeof(line), stdin);
+
+    // loop through each character of the string
+    for (int i = 0; line[i] != '\0'; ++i)
+    {
+
+        // convert character to lowercase
+        line[i] = tolower(line[i]);
+
+        // check if the character is a vowel
+        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+            line[i] == 'o' || line[i] == 'u')
+        {
+
+            // increment value of vowels by 1
+            ++vowels;
+        }
+
+        // if it is not a vowel and if it is an alphabet, it is a consonant
+        else if ((line[i] >= 'a' && line[i] <= 'z'))
+        {
+            ++consonant;
+        }
+
+        // check if the character is a digit
+        else if (line[i] >= '0' && line[i] <= '9')
+        {
+            ++digit;
+        }
+
+        // check if the character is an empty space
+        else if (line[i] == ' ')
+        {
+            ++space;
+        }
+    }
+
+    printf("\nVowels: %d", vowels);
+    printf("\nConsonants: %d", consonant);
+    printf("\nDigits: %d", digit);
+    printf("\nWhite spaces: %d", space);
+    printf("\n------------------------------------------------------------------------\n");
+    return 0;
+}
+
+// main -----------------------------------------------------------------------------------------------------------
 int main()
 {
-    printf("Main Menu");
+    printf("\n------------------------------------------------------------------------\n");
+    printf("\t---\t\t\t  Main Menu  \t\t\t---");
+    printf("\n------------------------------------------------------------------------\n");
     printf("\n\t1.Equality");
     printf("\n\t2.String Copy");
     printf("\n\t3.Concat");
-    printf("\n\t4.Show");
+    printf("\n\t4.length_string");
     printf("\n\t5.Reverse");
     printf("\n\t6.Palindrone");
-    printf("\n\t7.Sub String");
-    printf("\n\t8.Exit\n");
-
-    // do
-    // {
-    //     int num;
-    //     printf("\nEnter Your Choics :");
-    //     scanf("%d", &num);
-    // } while (num == fu)
-    // {
-    //     epuality();
-    //     stringCopy();
-    //     concat();
-    // length_string();
-    //     reverse();
-    //     palindrone();
-    // }
+    printf("\n\t7.srequency_character_s_string");
+    printf("\n\t8.number_blank_spaces_vowels_consonants_digits");
+    printf("\n\t0.Exit");
+    printf("\n------------------------------------------------------------------------\n");
 
     int num;
-    printf("\nEnter Your Choics :");
-    scanf("%d", &num);
-    switch (num)
+    while (num != 0)
     {
-    case 1:
-        epuality();
-        break;
-    case 2:
-        stringCopy();
-        break;
-    case 3:
-        concat();
-        break;
-    case 4:
-        length_string();
-        break;
-    case 5:
-        reverse();
-        break;
-    case 6:
-        palindrone();
-        break;
-    default:
-        printf("athar numbar are repited");
-        break;
+        printf("\n---Usar info Functionality Access By 1 to 8 and 0 Numbar are Exit---");
+        printf("\nEnter Your Choics : ");
+        scanf("%d", &num);
+        switch (num)
+        {
+        case 1:
+            epuality();
+            break;
+        case 2:
+            stringCopy();
+            break;
+        case 3:
+            concat();
+            break;
+        case 4:
+            length_string();
+            break;
+        case 5:
+            reverse();
+            break;
+        case 6:
+            palindrone();
+            break;
+        case 7:
+            srequency_character_s_string();
+            break;
+        case 8:
+            number_blank_spaces_vowels_consonants_digits();
+            break;
+        default:
+            printf("\n------------------------------------------------------------------------\n");
+            printf("\t---\t\t\tThanku\t\t\t ---");
+            printf("\n------------------------------------------------------------------------\n");
+            break;
+        }
     }
-
     return 0;
 }
